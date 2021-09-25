193d5b9930105a5542dde61a3b17efa1d87196af
Carlos <carlosesp10@gmail.com>
2021-09-25 08:39:44 -0500

    Se agregan carpetas assets, components y layouts

---
+++ src/components/functions.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const functions = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+functions.propTypes = {
+
+}
+
+export default functions
---
+++ src/layouts/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const footer = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+footer.propTypes = {
+
+}
+
+export default footer
