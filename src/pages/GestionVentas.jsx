810acafba767454f591fd04eed5cb0c887628cb7
Valentina Forero <valeforero98@gmail.com>
2021-09-27 20:26:10 -0500

    se agrega selector de los productos en la gestion de ventas

--- src/pages/GestionVentas.jsx
+++ src/pages/GestionVentas.jsx
@@ -3,7 +3,8 @@ import Input from 'components/Input';
 import {Formulario} from 'elements/Formularios';
 import Expresiones from 'components/Expresiones';
 import BotonCentrado from 'components/BotonCentrado';
-import AlertaError from 'components/AlertaError';
+import AlertaError from 'components/AlertaError'
+import Select from 'react-select';
 
 const GestionVentas = () => {
     const [nombre, cambiarNombre] = useState({campo:'', valido: null});
@@ -11,7 +12,14 @@ const GestionVentas = () => {
     const [documento, cambiarDocumento] = useState({campo:'', valido: null});
     const [idVendedor, cambiarIdVendedor] = useState({campo:'', valido: null});
     const [fecha, cambiarFecha] = useState({campo:'', valido: null});
+    const [cantidadProducto, cambiarCantidadProducto] = useState({campo:'', valido: null});
     const [formularioValido, cambiarFormularioValido] = useState(null);
+    const foodOptions = [
+        {value: 'comida-perros-adultos', label: "Comida perros adultos"},
+        {value: 'comida-perros-cachorros', label: "Comida perros cachorros"},
+        {value: 'comida-perros-pelo-delgado', label: "Comida perros pelo delgado"}
+
+    ]
     const onSubmitForm = (e) =>{
         e.preventDefault();
         if (
@@ -19,7 +27,8 @@ const GestionVentas = () => {
             apellido.valido === 'true' &&
             documento.valido === 'true' &&
             idVendedor.valido === 'true' &&
-            fecha.valido === 'true' 
+            fecha.valido === 'true' && 
+            cantidadProducto.valido === 'true' 
             ){
                 cambiarFormularioValido(true);
                 cambiarNombre({campo: '', valido:''});
@@ -27,6 +36,7 @@ const GestionVentas = () => {
                 cambiarDocumento({campo: '', valido:''});
                 cambiarIdVendedor({campo: '', valido:''});
                 cambiarFecha({campo: '', valido:''});
+                cambiarCantidadProducto({campo: '', valido:''});
                 // hacer envios a apis base de datos
             }else{
                 cambiarFormularioValido(false);
@@ -85,6 +95,24 @@ const GestionVentas = () => {
                     name = "idVendedor"
                     estado = {idVendedor}
                     cambiarEstado = {cambiarIdVendedor}
+                 />
+                 <div>
+                    <Select
+                        options={foodOptions}
+                        placeholder = "Seleccione el producto"
+                        isSearchable                     
+                    />
+                     <button>+</button>
+                 </div>
+                 <Input 
+                    user = "cantidadProducto"
+                    placeholdercont = "cantidadProducto"
+                    tipo = "number"
+                    lenyenda = "Solo ingrese numeros para asignar una cantidad al producto"
+                    expresionRegular = {Expresiones.telefono}
+                    name = "cantidadProducto"
+                    estado = {cantidadProducto}
+                    cambiarEstado = {cambiarCantidadProducto}
                  />
                  {formularioValido === false  && <AlertaError/>}
                 <BotonCentrado 
