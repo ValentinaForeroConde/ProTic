987bd3535aff2bff02e4863e6ffadc86134cdb75
Valentina Forero <valeforero98@gmail.com>
2021-10-08 20:45:11 -0500

    Se crea CRUD de productos(vistas y controladores)

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,7 @@ import Cors from 'cors';
 import dotenv from 'dotenv';
 import { conectarBD } from './db/db.js';
 import rutasUsuario from './views/usuarios/rutas.js';
+import rutasProductos from './views/productos/rutas.js';
 
 dotenv.config({path: './.env'});
 
@@ -11,6 +12,7 @@ const app = Express();
 app.use(Express.json());
 app.use(Cors());
 app.use(rutasUsuario);
+app.use(rutasProductos);
 
 const main = () => {
     return app.listen(process.env.PORT, () => {
